83aff61b087e83a548fe71d70b279c7c0f48f4a9
buraktnv <burk__39@hotmail.com>
2022-06-21 22:23:09 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="w-4/5 mx-auto mt-36 grid grid-cols-4">
+    <div className="w-4/5 mx-auto mt-8 grid grid-cols-4">
       <Sidebar />
       <div className="col-span-3">
         <Content />
